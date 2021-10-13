f5e6fc0fab3580dbbf11ef5a121cf19c68197ede
tarikyavuzca <tarikyavuzca@gmail.com>
2021-10-13 18:11:32 -0400

    timeago 'date time formatting updated,  bugs are fixed

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -16,6 +16,37 @@ const renderTweets = tweetsDatabase => {
   });
 };
 
+
+
+// createTweetElement function accepting tweetData as a parameter and returns the structure that containing tweets
+const createTweetElement = tweetData => {
+  
+  const $tweet = $('<article>').addClass('tweet');
+  
+  const $dayAgo = (tweetData.created_at);
+  
+  // Elements of tweet object
+  const htmlContent = `
+  <header>
+  <img src=${escape(tweetData.user.avatars)} alt="${escape(tweetData.user.handle)}-avatar">
+  <span>${escape(tweetData.user.name)}</span>
+  <span>${escape(tweetData.user.handle)}</span>
+  </header>
+  <p>${escape(tweetData.content.text)}</p>
+  <footer>
+  ${timeago.format($dayAgo)}
+  <span>
+  <i class="fas fa-flag"></i>
+  <i class="fas fa-retweet"></i>
+  <i class="fas fa-heart"></i>
+  </span>
+  </footer>
+  `
+  return $tweet.html(htmlContent);
+};
+
+
+
 // Making a GET request to the database
 // runs the returned tweet 
 const loadTweets = () => {
@@ -28,63 +59,20 @@ const loadTweets = () => {
 };
 
 
-// daysSinceTweet function takes a  parameter of timeOfTweet
-const daysSinceTweet = function(timeOfTweet) {
-  const currentDate = new Date();
-  const currentTime = currentDate.getTime();
-  const millisecondsInDay = 86400000;
-
-  const timeDifference = currentTime - timeOfTweet;
-  const dayDifference = timeDifference / millisecondsInDay;
-
-  return Math.floor(dayDifference);
-};
-
-
-// createTweetElement function accepting tweetData as a parameter and returns the structure that containing tweets
-const createTweetElement = tweetData => {
-
-  const $tweet = $('<article>').addClass('tweet');
-
-  const $dayAgo = daysSinceTweet(tweetData.created_at);
-
-  // Elements of tweet object
-  const htmlContent = `
-    <header>
-      <img src=${escape(tweetData.user.avatars)} alt="${escape(tweetData.user.handle)}-avatar">
-      <span>${escape(tweetData.user.name)}</span>
-      <span>${escape(tweetData.user.handle)}</span>
-    </header>
-    <p>${escape(tweetData.content.text)}</p>
-    <footer>
-      ${$dayAgo} days ago
-      <span>
-        <i class="fas fa-flag"></i>
-        <i class="fas fa-retweet"></i>
-        <i class="fas fa-heart"></i>
-      </span>
-    </footer>
-  `
-  return $tweet.html(htmlContent);
-};
-
-
-
-
-
 $(document).ready(() => { //When the page is loaded
-
+  
+  
   
   // shows/hides new tweet section when clicked the arrow icon on navbar
-    $('nav i').on('click', () => {
+  $('nav i').on('click', () => {
     $('.new-tweet').slideToggle();
     $('.new-tweet textarea').focus();
   });
-
+  
   // handling new tweet form submit
   $('.new-tweet form').submit(function(event) {
     event.preventDefault();
-
+    
     
     const $errorMessage = $(this).children('.error-message');
     const $text = $(this).children('textarea');
@@ -92,42 +80,43 @@ $(document).ready(() => { //When the page is loaded
     
     // hide error message in case it's in display
     $errorMessage.hide();
-
+    
     // check if message is exist
     if (!message) {
       $(".error-message")
-        .html("<i class='fas fa-exclamation-triangle'></i>  Use (.) :) !<i class='fas fa-exclamation-triangle'></i> ")
-        .slideDown("slow")
-        .delay(1500)
-        .slideUp("slow");
+      .html("<i class='fas fa-exclamation-triangle'></i>  Use (.) :) !<i class='fas fa-exclamation-triangle'></i> ")
+      .slideDown("slow")
+      .delay(1500)
+      .slideUp("slow");
       
-    // check if the length of message is greater than 140
+      // check if the length of message is greater than 140
     } else if (message.length > 140) {
       $(".error-message")
-        .html("<i class='fas fa-exclamation-triangle'></i> Dude, don't you see the limit!<i class='fas fa-exclamation-triangle'></i> ")
-        .slideDown("slow")
-        .delay(1500)
-        .slideUp("slow");
-
-    // else everything goes smooth 
+      .html("<i class='fas fa-exclamation-triangle'></i> Dude, don't you see the limit!<i class='fas fa-exclamation-triangle'></i> ")
+      .slideDown("slow")
+      .delay(1500)
+      .slideUp("slow");
+      
+      // else everything goes smooth 
     } else { 
       $(".tweet-good")
-        .html("<i class='fas fa-smile-wink'></i> Tweet Sent, Check Below <i class='fas fa-smile-wink'></i> ")
-        .slideDown("slow")
-        .delay(1500)
-        .slideUp("slow");
+      .html("<i class='fas fa-smile-wink'></i> Tweet Sent, Check Below <i class='fas fa-smile-wink'></i> ")
+      .slideDown("slow")
+      .delay(1500)
+      .slideUp("slow");
       
       // Getting the tweets from the server and prompt them on the page
-        $.ajax('/tweets', {
-          method: 'POST',
-          data: $(this).serialize(),
-          success: () => { 
+      $.ajax('/tweets', {
+        method: 'POST',
+        data: $(this).serialize(),
+        success: () => { 
           loadTweets(); 
           $text.val('');
           $('.counter').html("140");
-          }, 
-          error: (data, text, error) => console.error(error)
-        })
+        }, 
+        error: (data, text, error) => console.error(error)
+      })
+      loadTweets();
     }
   })
 })
